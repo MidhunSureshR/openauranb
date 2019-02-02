e102aece68d57b9c064737a6052e5ba93ca2f677
MidhunSureshR <thedeceptionist@hotmail.com>
2019-02-02 10:15:10 +0530

    Added CMake and also also added code to find the HID device procedurally.

---
+++ cmake-build-debug/CMakeFiles/openauranb.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/openauranb.dir/rog_aura.c.o"
+  "openauranb.pdb"
+  "openauranb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/openauranb.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
